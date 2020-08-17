5b6c61dc92565415eb559b1d7614363d3433ca6d
manzaloros <zacharymansellcello@gmail.com>
2020-08-17 16:54:11 -0700

    Add div for pictures, add static file path

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 2999;
 
-app.use(express.static('../'));
+app.use(express.static(path.join(__dirname, '../', '/public')));
 
 app.get('/', (req, res) => {
   res.send('Hello from server!');
