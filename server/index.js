94d19ea104ffc56a68e8c6ec3980941ae3ba5103
manzaloros <zacharymansellcello@gmail.com>
2020-08-17 20:43:02 -0700

    Reinstall http-proxy express package

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 2999;
 
-app.use(express.static(path.join(__dirname, '../', '/public')));
+app.use('/:id', express.static(path.join(__dirname, '../', '/public')));
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello from server!');
-});
+// app.get('/:id/api/:id', (req, res) => {
+//   res.send('Hello from server!');
+// });
 
 app.listen(port, () => {
   console.log(`Proxy server listening at http://localhost:${port}`);
