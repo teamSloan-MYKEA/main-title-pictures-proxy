0f54d6b0fb2a9e5b6278a57f0e02d99f484f2e47
manzaloros <zacharymansellcello@gmail.com>
2020-08-18 12:34:03 -0700

    Add 3003 to app.use, addtobag and similarproducts In index.html

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use('/:id', express.static(path.join(__dirname, '../', '/public')));
 app.use(cors());
 
 app.use('/:id', createProxyMiddleware({ target: 'http://localhost:3000/' }));
+app.use('/:id', createProxyMiddleware({ target: 'http://localhost:3003/' }));
 
 // app.get('/:id/api/:id', (req, res) => {
 //   res.send('Hello from server!');
