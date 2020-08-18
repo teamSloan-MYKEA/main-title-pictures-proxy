b708e87e5415b3f3e56a1f289375a95f0e5f4f31
manzaloros <zacharymansellcello@gmail.com>
2020-08-18 11:24:57 -0700

    Add proxy middleware use to pictures url

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const port = process.env.PORT || 2999;
 
 app.use('/:id', express.static(path.join(__dirname, '../', '/public')));
 app.use(cors());
+
+app.use('/:id', createProxyMiddleware({ target: 'http://localhost:3000/' }));
 
 // app.get('/:id/api/:id', (req, res) => {
 //   res.send('Hello from server!');
