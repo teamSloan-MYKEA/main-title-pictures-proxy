3347d3b461b6df33e94e5b03339f0da619260ed3
manzaloros <zacharymansellcello@gmail.com>
2020-08-21 20:17:18 -0700

    Change pictures ip's to use new server ip

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use('/:id', express.static(path.join(__dirname, '../', '/public')));
 // Not sure if CORS is needed:
 app.use(cors());
 
-app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://18.190.28.33:3000/' }));
+app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://3.15.203.117:3000/' }));
 app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://52.207.78.191/' }));
 app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003/' }));
 app.use('/:id/reviews/:id', createProxyMiddleware({ target: 'http://18.223.24.49:3002/' }));
