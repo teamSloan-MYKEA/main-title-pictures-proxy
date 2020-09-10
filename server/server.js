ea0d45aa28944e87a712dc678bc216065f2c6a50
EC2 Default User <ec2-user@ip-172-31-44-224.us-east-2.compute.internal>
2020-09-10 20:48:20 +0000

    Optimization, page insights, loader.io

--- server/server.js
+++ server/server.js
@@ -4,11 +4,17 @@ const express = require('express');
 const app = express();
 const port = 2999;
 const servicePort = 3001;
+const compression = require('compression');
 const path = require('path');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'public, max-age=31557600, s-maxage=31557600');
+  next();
+});
+app.use(compression());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 app.use(('/'), express.static(path.join(__dirname, '../public')));
@@ -17,8 +23,8 @@ app.use(('/:id'), express.static(path.join(__dirname, '../client')));
 app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://18.220.212.160:3000'}));
 //app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://localhost:3000'}));
 //app.use('/:id/pictures/:id', createProxyMiddleware({ target: 'http://3.15.203.117:3000' }));
-app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://52.207.78.191' }));
-app.use('/:id/reviews/:id', createProxyMiddleware({ target: 'http://18.223.24.49:3002' }));
+app.use('/:id/similar/:id', createProxyMiddleware({ target: 'http://54.177.81.158:3001' }));
+app.use('/:id', createProxyMiddleware({ target: 'http://3.21.37.48' }));
 // app.use('/:id/bag/:id', createProxyMiddleware({ target: 'http://localhost:3003' }));
 
 
